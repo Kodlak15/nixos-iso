96653a9617b44d4345b0546660568c6e4acaeee0
Kodlak15 <stanlcod15@gmail.com>
2024-06-23 19:37:14 -0700

    Set up ssh auth

--- minimal/default.nix
+++ minimal/default.nix
@@ -16,7 +16,14 @@ in {
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   services = {
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      ports = [22];
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
   };
 
   networking = {
@@ -29,7 +36,21 @@ in {
     gpgSmartcards.enable = true;
   };
 
+  users.users = {
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFPX5biiGhDszrPE8BWvgKz4Ow1etck3E9aacuKXObQ cody@everest"
+      ];
+    };
+  };
+
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
+
+  # Should make build go faster
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+
   environment.systemPackages = with pkgs; [
+    neofetch
     neovim
     tmux
     # For setting up Yubikey based FDE
